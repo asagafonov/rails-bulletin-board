9f25de383e97a95e6270bf8247b2d2fbf1157a34
asagafonov <agafonov@buroburo.ru>
2023-02-01 18:31:30 +0300

    add fields length validation

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -6,7 +6,8 @@ class Bulletin < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :title, :description, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :image, attached: true,
                     content_type: %i[png jpg jpeg],
                     size: { less_than: 5.megabytes }
