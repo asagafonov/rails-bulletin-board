9ec1d5ada98a90960c76d0fa6c9a16e01a6f99de
asagafonov <a.s.agafonov@yandex.ru>
2023-02-01 09:43:30 +0300

    add secret_key_base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.secret_key_base = ENV.fetch('SECRET_KEY_BASE', nil)
 end
