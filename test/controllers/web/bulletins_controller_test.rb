9ad8b5c30b7f8bb2b4874992b48b823f4736fec7
asagafonov <a.s.agafonov@yandex.ru>
2023-02-01 09:48:03 +0300

    remove default tests

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -1,38 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class BulletinsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get bulletins_index_url
-    assert_response :success
-  end
+require 'test_helper'
 
-  test "should get show" do
-    get bulletins_show_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get bulletins_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get bulletins_create_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get bulletins_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get bulletins_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get bulletins_destroy_url
-    assert_response :success
-  end
+class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
 end
