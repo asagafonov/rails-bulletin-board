033ff01bd8f6dcb9baed3fe1231acf71e62a1f3f
asagafonov <agafonov@buroburo.ru>
2023-02-01 09:24:09 +0300

    upd routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api
-
   scope module: :web do
-    namespace :admin
-
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 
     resource :bulletins
-
-    root 'bulletins#index'
   end
+
+  root 'web/bulletins#index'
 end
+
