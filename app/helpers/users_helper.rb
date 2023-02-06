eaa07ce4318a1e78dda66faa802e948ff5f01dea
asagafonov <a.s.agafonov@yandex.ru>
2023-02-06 14:31:27 +0300

    add state translation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/MethodLength
+
 module UsersHelper
+  def self.translate_state(bulletin_state)
+    case bulletin_state
+    when 'draft'
+      ::I18n.t('bulletins.state.draft')
+    when 'under_moderation'
+      ::I18n.t('bulletins.state.under_moderation')
+    when 'published'
+      ::I18n.t('bulletins.state.published')
+    when 'archived'
+      ::I18n.t('bulletins.state.archived')
+    else
+      ''
+    end
+  end
 end
+
+# rubocop:enable Metrics/MethodLength
