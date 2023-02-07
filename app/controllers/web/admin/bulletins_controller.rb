303f691f7e77dd9a53c231fd97dbb37905ff4e86
asagafonov <a.s.agafonov@yandex.ru>
2023-02-07 11:19:20 +0300

    add missing views

--- app/controllers/web/admin/bulletins_controller.rb
+++ app/controllers/web/admin/bulletins_controller.rb
@@ -10,55 +10,11 @@ module Web
 
       def moderation
         @bulletins_on_moderation = Bulletin.includes(:user).under_moderation.by_creation_date_desc
-        pp @bulletins_on_moderation
         @page_type = :bulletins_on_moderation
       end
 
       def show
         @bulletin = find_bulletin
-      end
-
-      def new
-        @bulletin = Bulletin.new
-        authorize @bulletin
-      end
-
-      def create
-        @bulletin = Bulletin.create(bulletin_params)
-        authorize @bulletin
-
-        if @bulletin.save
-          redirect_to @bulletin, notice: t('bulletins.create.success')
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
-
-      def edit
-        @bulletin = find_bulletin
-        authorize @bulletin
-      end
-
-      def update
-        @bulletin = find_bulletin
-        authorize @bulletin
-
-        if @bulletin.update(bulletin_params)
-          redirect_to @bulletin, notice: t('bulletins.update.success')
-        else
-          render :edit, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @bulletin = find_bulletin
-        authorize @bulletin
-
-        if @bulletin.destroy
-          redirect_to bulletins_path, notice: t('bulletins.destroy.success')
-        else
-          redirect_to @bulletin, alert: t('bulletins.destroy.failure')
-        end
       end
 
       def update_state
@@ -72,10 +28,6 @@ module Web
 
       def find_bulletin
         Bulletin.find(params[:id])
-      end
-
-      def bulletin_params
-        params.require(:bulletin).permit(:category_id, :description, :image, :title, :user_id)
       end
     end
   end
