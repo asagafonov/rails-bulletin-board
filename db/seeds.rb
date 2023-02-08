23b55f2d4cd7acf94990fb9431a259a225baa351
asagafonov <agafonov@buroburo.ru>
2023-02-08 15:30:01 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
     name: Faker::Commerce.unique.brand
   )
 end
+
+User.create(
+  name: 'Andrey Agafonov',
+  email: 'a.s.agafonov@yandex.ru',
+  admin: true
+)
+
+100.times do
+  bulletin = Bulletin.create(
+    title: Faker::Lorem.sentence(word_count: 2),
+    description: Faker::Lorem.paragraph,
+    user_id: User.find_by(email: 'a.s.agafonov@yandex.ru').id,
+    category_id: Category.all.map(&:id).sample,
+    state: %w[draft under_moderation published rejected archived].sample
+  )
+  bulletin.image.attach(
+    io: File.open(File.join(Rails.root, 'app/assets/images/template_photo.png')),
+    filename: 'template_photo.png',
+    content_type: 'image/png'
+  )
+  bulletin.save!
+end
