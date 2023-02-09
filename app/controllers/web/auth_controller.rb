2fd36485ad83e55d2db4ac111d4efee6aa9591a4
asagafonov <a.s.agafonov@yandex.ru>
2023-02-09 19:09:57 +0300

    fix linter issues

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/AbcSize
-
 module Web
   class AuthController < ApplicationController
     def callback
       name = auth[:name] || nil
       email = auth[:email]
-      user = User.find_by(email:)
 
-      if user&.persisted?
-        session[:user_id] = user.id
-      else
-        new_user = User.create!(name:, email:)
-        session[:user_id] = new_user.id
-      end
+      redirect_to root_path, alert: t('views.auth.actions.login.failure') and return unless email
+
+      add_user_to_session(name, email)
 
       redirect_to root_path, notice: t('views.auth.actions.login.success')
     end
@@ -26,11 +20,20 @@ module Web
 
     private
 
+    def add_user_to_session(name, email)
+      user = User.find_by(email:)
+
+      if user&.persisted?
+        session[:user_id] = user.id
+      else
+        new_user = User.create!(name:, email:)
+        session[:user_id] = new_user.id
+      end
+    end
+
     def auth
       data = request.env['omniauth.auth']
       { email: data[:info][:email], name: data[:info][:name] }
     end
   end
 end
-
-# rubocop:enable Metrics/AbcSize
