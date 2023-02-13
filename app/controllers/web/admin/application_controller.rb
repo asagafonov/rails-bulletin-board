290809e1b9fbee131d1dc6d90907c5420f12d14a
asagafonov <a.s.agafonov@yandex.ru>
2023-02-13 13:51:00 +0300

    add category behavior on delete restriction

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -2,5 +2,12 @@
 
 module Web
   class Admin::ApplicationController < ApplicationController
+    rescue_from ActiveRecord::DeleteRestrictionError, with: :not_permitted
+
+    private
+
+    def not_permitted
+      redirect_to admin_categories_path, alert: t('categories.delete_restriction_error')
+    end
   end
 end
