ee86403d4c56d10562b6bb3de722fb275d433163
asagafonov <agafonov@buroburo.ru>
2023-02-14 12:36:59 +0300

    add admin rights check before controller action

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -2,12 +2,17 @@
 
 module Web
   class Admin::ApplicationController < ApplicationController
+    before_action :check_admin_rights
     rescue_from ActiveRecord::DeleteRestrictionError, with: :not_permitted
 
     private
 
     def not_permitted
       redirect_to admin_categories_path, alert: t('categories.delete_restriction_error')
+    end
+
+    def check_admin_rights
+      redirect_to root_path, alert: t('not_admin_error') unless current_user&.admin?
     end
   end
 end
